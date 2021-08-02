ea7c87c490bb2ff25610ee1e0afceda33cd42b4f
Jie Zhou (JAY) <jiz156@pitt.edu>
2021-08-02 15:31:23 -0400

    Add in validation for the form

--- forms-demo/src/app/user-setting-form/user-setting-form.component.ts
+++ forms-demo/src/app/user-setting-form/user-setting-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm, NgModel } from '@angular/forms';
 import { UserSettings } from '../data/user-settings';
 
 @Component({
@@ -8,17 +9,27 @@ import { UserSettings } from '../data/user-settings';
 })
 export class UserSettingFormComponent implements OnInit {
 
-  userSettings : UserSettings = {
-    name: 'Milton',
-    emailOffers: true,
-    interfaceStyle: 'dark',
-    subscriptionType: 'Annual',
-    notes: 'here are some notes...'
+  originalUserSettings : UserSettings = {
+    name: null,
+    emailOffers: null,
+    interfaceStyle: null,
+    subscriptionType: null,
+    notes: null
   }
+
+  userSettings : UserSettings = { ...this.originalUserSettings }
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm){
+    console.log('in onSubmit: ', form.valid);
+  }
+
+  onBlur(field: NgModel){
+    console.log('in onBlur: ', field.valid);
   }
 
 }
