f5706d6eb859744a2dfb040c10f4e8b3aa718a24
Jie Zhou (JAY) <jiz156@pitt.edu>
2021-08-02 13:54:28 -0400

    add two way binding

--- forms-demo/src/app/user-setting-form/user-setting-form.component.ts
+++ forms-demo/src/app/user-setting-form/user-setting-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserSettings } from '../data/user-settings';
 
 @Component({
   selector: 'app-user-setting-form',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-setting-form.component.css']
 })
 export class UserSettingFormComponent implements OnInit {
+
+  userSettings : UserSettings = {
+    name: 'Milton',
+    emailOffers: true,
+    interfaceStyle: 'dark',
+    subscriptionType: 'Annual',
+    notes: 'here are some notes...'
+  }
 
   constructor() { }
 
