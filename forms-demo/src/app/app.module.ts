27993def8e8a5c17ed2c2fca5a07f90913a9bdec
Jie Zhou (JAY) <jiz156@pitt.edu>
2021-08-02 17:14:18 -0400

    submit a form though fake http request and respond

--- forms-demo/src/app/app.module.ts
+++ forms-demo/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { UserSettingFormComponent } from './user-setting-form/user-setting-form.component';
@@ -13,6 +14,7 @@ import { UserSettingFormComponent } from './user-setting-form/user-setting-form.
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
